23d078ea596db8ee04537fa9de58b3c44d89b1bc
Aidarbek Kabylbekov <miligvez@gmail.com>
2023-07-12 21:21:53 -0400

    added role and properties models

--- airbnb/models.py
+++ airbnb/models.py
@@ -19,3 +19,25 @@ class User(models.Model):
     
     def is_super_host(self):
         return self.is_superhost
+    
+class Role(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.role
+
+class Properties(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    property_name = models.CharField(max_length=50)
+    lat_long = models.CharField(max_length=255)
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    zipcode = models.CharField(max_length=100)
+    desc = models.CharField(max_length=1000)
+    photo = models.CharField(max_length=255)
+    guests_allowed = models.IntegerField()
+    bedrooms = models.IntegerField()
+    bathrooms = models.IntegerField()
+    beds = models.IntegerField()
